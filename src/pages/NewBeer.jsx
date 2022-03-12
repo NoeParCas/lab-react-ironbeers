564a89fa2da04428f02a4628fc8ee514c91db948
Noelia Parada <noeliacastrelo@gmail.com>
2022-03-12 18:16:10 +0100

    iteraccion 5 y 6 hechas

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,11 +1,99 @@
 import React from 'react'
 import Header from '../components/Header'
+import { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 function NewBeer() {
+//1. crear los estados que manejan la información
+const [ name, setName ] = useState("")
+const [ tagline, setTagline ] = useState("")
+const [ description, setDescription ] = useState("")
+const [ firstBrewed, setFirstBrewed ] = useState("")
+const [ brewersTips, setBrewersTips ] = useState("")
+const [ attenuationLevel, setAttenuationLevel ] = useState(0)
+const [ contributedBy, setContributedBy ] = useState("")
+const navigate = useNavigate()
+
+
+const handleSubmit = async(event) => {
+  //prevenir el comportamiento del form
+  event.preventDefault()
+  //elemento que está creando el usuario
+  const newBeer = { name, tagline, description, firstBrewed, brewersTips, attenuationLevel, contributedBy}
+  await axios.post("https://ih-beers-api2.herokuapp.com/beers/new", newBeer)
+
+  //redireccionar el usuario a "/beers"
+  navigate("/beers")
+}
+
+
   return (
     <div>
     <Header/>
         <h2>New Beer</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input 
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <label htmlFor="tagline">Tagline</label>
+          <input 
+            type="text"
+            name="tagline"
+            value={tagline}
+            onChange={(e) => setTagline(e.target.value)}
+          />
+          <br />
+          <label htmlFor="description">Description</label>
+          <input 
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <br />
+          <label htmlFor="firstBrewed">First Brewed</label>
+          <input 
+            type="text"
+            name="firstBrewed"
+            value={firstBrewed}
+            onChange={(e) => setFirstBrewed(e.target.value)}
+          />
+          <br />
+          <label htmlFor="brewersTips">Brewers Tips</label>
+          <input 
+            type="text"
+            name="brewersTips"
+            value={brewersTips}
+            onChange={(e) => setBrewersTips(e.target.value)}
+          />
+          <br />
+          <label htmlFor="attenuationLevel">Attenuation Level</label>
+          <input 
+            type="number"
+            name="attenuationLevel"
+            value={attenuationLevel}
+            onChange={(e) => setAttenuationLevel(e.target.value)}
+          />
+          <br />
+          <label htmlFor="contributedBy">Contributed by</label>
+          <input 
+            type="text"
+            name="contributedBy"
+            value={contributedBy}
+            onChange={(e) => setContributedBy(e.target.value)}
+          />
+          <br />
+          <button>ADD NEW</button>
+
+
+
+        </form>
     </div>
   )
 }
--- src/pages/RandomBeer.jsx
+++ src/pages/RandomBeer.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function RandomBeer() {
-  return (
-    <div>
-     <Header/>
-        
-    </div>
-  )
+  //1. crear el estado que maneja la información
+  const [randomBeer, setRandomBeer] = useState(null);
+
+  //2. useEffect que llamará al componentDidMount
+  useEffect(() => {
+   getRandomBeer()
+  }, []);
+
+  //3. funcion que llama la data de la Api
+  const getRandomBeer = async() => {
+    const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers/random")
+    // console.log(response.data)
+    setRandomBeer(response.data)
 }
 
-export default RandomBeer
+if(!randomBeer) return <h2>...is Loading</h2>
+
+  return (
+    <div>
+      <Header />
+      <h2>Random Beer</h2>
+      <img src={randomBeer.image_url} alt={randomBeer.name} width='50px' />
+      <h3>{randomBeer.name}</h3>
+      <p>{randomBeer.tagline}</p>
+      <p>{randomBeer.first_brewed}</p>
+      <p>{randomBeer.attenuation_level}</p>
+      <p>{randomBeer.description}</p>
+      <p>{randomBeer.contributed_by}</p>
+    </div>
+  );
+}
+
+export default RandomBeer;
